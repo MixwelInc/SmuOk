9e6824c6cd36e3382256058416b9fdd4cf84f50f
Makar <smakar20001@gmail.com>
2022-10-07 23:13:12 +0300

    code clear

--- Component/Log.cs
+++ Component/Log.cs
@@ -16,11 +16,6 @@ namespace SmuOk.Component
 
     private void Log_Load(object sender, EventArgs e)
     {
-      /*string q = "select SVName + " +
-          " case when SVNo=0 then ' (изменений не было)' else ' (вер. ' + SVNo + ')' end +', получено: ' + " +
-          "	convert(nvarchar, SVDate, 104) + ', строк: '  +case when NewestFillingCount = 0 then 'нет' else convert(nvarchar, NewestFillingCount) end f " +
-          " from vwSpec " +
-          " where SId=" + SpecId;*/
       string q = "select SVName from vwSpec where SId=" + SpecId;
       string s = (string)MyGetOneValue(q);
       lblTitle.Text = s;
