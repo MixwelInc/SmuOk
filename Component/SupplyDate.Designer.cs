d2d5d89928b57182f3243b3db122002ef6e9e487
Makar <smakar20001@gmail.com>
2022-12-18 19:36:22 +0300

    Added instruction on SupplyDate

--- Component/SupplyDate.Designer.cs
+++ Component/SupplyDate.Designer.cs
@@ -80,6 +80,7 @@
             this.label1 = new System.Windows.Forms.Label();
             this.SFEOId = new System.Windows.Forms.TextBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSpecSupplyDateFill)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.dgvSpec)).BeginInit();
             this.SuspendLayout();
@@ -628,10 +629,23 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.label2.Location = new System.Drawing.Point(986, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 15);
+            this.label2.TabIndex = 81;
+            this.label2.Text = "Инструкция";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
             // SupplyDate
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -711,5 +725,6 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox SFEOId;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
